9afb47db00c846cd84978cd71c50ea077feabb16
valechow <valechowseg@gmail.com>
2024-12-09 22:19:35 -0600

    Vista de Registro de Actividad y Auditoría

--- ASECCC Digital/ASECCC Digital/Controllers/SeguridadyPermisosController.cs
+++ ASECCC Digital/ASECCC Digital/Controllers/SeguridadyPermisosController.cs
@@ -30,12 +30,50 @@ namespace ASECCC_Digital.Controllers
 
         public ActionResult RegistroActividadAuditoria()
         {
-            return View();
+            var actividades = ObtenerRegistroDeActividad();
+            return View(actividades);
+      
+        }
+
+        private List<Actividad> ObtenerRegistroDeActividad()
+        {
+            // Simulación de datos para la tabla de auditoría
+            return new List<Actividad>
+            {
+                new Actividad
+                {
+                    Usuario = "Admin1",
+                    Rol = "Administrador",
+                    FechaAcceso = DateTime.Now.Date,
+                    HoraAcceso = DateTime.Now.AddHours(-2)
+                },
+                new Actividad
+                {
+                    Usuario = "User2",
+                    Rol = "Asociado",
+                    FechaAcceso = DateTime.Now.Date,
+                    HoraAcceso = DateTime.Now.AddHours(-1)
+                },
+                new Actividad
+                {
+                    Usuario = "Admin2",
+                    Rol = "Administrador",
+                    FechaAcceso = DateTime.Now.Date,
+                    HoraAcceso = DateTime.Now.AddHours(-3)
+                }
+            };
         }
 
 
 
 
         //--------VISTAS USUARIO-------------//
+    }
+    public class Actividad
+    {
+        public string Usuario { get; set; }
+        public string Rol { get; set; }
+        public DateTime FechaAcceso { get; set; }
+        public DateTime HoraAcceso { get; set; }
     }
 }
