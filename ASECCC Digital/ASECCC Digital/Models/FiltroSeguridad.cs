a10074e554e3a425ced7429748e2e7a6d11cc766
Ebrenes22 <e-j-bm@outlook.com>
2025-01-19 21:07:04 -0600

    Agrego Filtro Seguridad
    - Filtro que verifica si un usuario está autenticado antes de permitirle acceder a una acción de un controlador si no lo esta, redirige al login

---
+++ ASECCC Digital/ASECCC Digital/Models/FiltroSeguridad.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ASECCC_Digital.Models
+{
+    public class FiltroSeguridad : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Session["NombreCompleto"] == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary
+                {
+                    { "controller", "Usuarios" },
+                    { "action", "Login" }
+                });
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
