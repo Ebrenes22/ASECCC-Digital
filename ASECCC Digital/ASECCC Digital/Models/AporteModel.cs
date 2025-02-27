70924d10a59d3f480f23d98814bd9a85a95c3a72
Carlos Bello <carlosbello3317@hotmail.com>
2025-02-27 01:01:07 -0600

    HU15 Admistrador CRUD Ahorros y Aportes
    Como administrador, quiero registrar, actualizar, y eliminar los ahorros y aportes de los asociados para mantener la información precisa y actualizada, y llevar un control detallado.

---
+++ ASECCC Digital/ASECCC Digital/Models/AporteModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASECCC_Digital.Models
+{
+    public class AporteModel
+    {
+        public int AporteId { get; set; }
+
+        [Required(ErrorMessage = "El usuario es requerido")]
+        public int UsuarioId { get; set; }
+
+        [Required(ErrorMessage = "El tipo de aporte es requerido")]
+        public string TipoAporte { get; set; }
+
+        [Required(ErrorMessage = "El monto es requerido")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El monto debe ser mayor a 0")]
+        public decimal Monto { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+    }
+}
