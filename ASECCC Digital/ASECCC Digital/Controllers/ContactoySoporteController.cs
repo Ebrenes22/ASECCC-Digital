57565239177c4e3cb4bec8d01db31cabb92c3106
AxelMCS1 <axelcmacho10@gmail.com>
2025-03-30 15:00:49 -0600

    FAQ finalizado

---
+++ ASECCC Digital/ASECCC Digital/Controllers/ContactoySoporteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASECCC_Digital.Controllers
+{
+    public class ContactoySoporteController : Controller
+    {
+
+
+        //--------VISTAS ASOCIADO-------------//
+        [HttpGet]
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+    }
+}
