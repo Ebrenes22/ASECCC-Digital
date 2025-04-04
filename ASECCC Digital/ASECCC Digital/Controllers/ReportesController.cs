b283e9758b8696e1f74f6f9eb509e2748b809584
Ebrenes22 <e-j-bm@outlook.com>
2025-04-03 21:05:08 -0600

    Actualización Estado de Cuenta

--- ASECCC Digital/ASECCC Digital/Controllers/ReportesController.cs
+++ ASECCC Digital/ASECCC Digital/Controllers/ReportesController.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ASECCC_Digital.Database;
+using ASECCC_Digital.ViewModels;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ASECCC_Digital.Controllers
@@ -14,7 +13,6 @@ namespace ASECCC_Digital.Controllers
         }
 
         //--------VISTAS ADMIN--------------//
-        // GET: Reportes
         public ActionResult Reporte()
         {
             return View();
@@ -22,17 +20,60 @@ namespace ASECCC_Digital.Controllers
 
         public ActionResult EstadodeCuentaAsociados()
         {
-            return View();
+            return RedirectToAction("GenerarEstadoAsociado", "Reportes");
         }
 
 
+        public ActionResult GenerarEstadoAsociado(int? UsuarioIdSeleccionado)
+        {
+            var model = new EstadoCuentaViewModel();
+
+            using (var context = new ASECCC_DIGITALEntities())
+            {
+                // Cargar usuarios para el dropdown
+                model.Usuario = context.Usuario
+                    .Select(u => new SelectListItem
+                    {
+                        Value = u.usuarioId.ToString(),
+                        Text = u.nombreCompleto
+                    }).ToList();
+
+                if (UsuarioIdSeleccionado.HasValue)
+                {
+                    model.UsuarioIdSeleccionado = UsuarioIdSeleccionado.Value;
+
+                    // Cargar el usuario completo
+                    model.UsuarioSeleccionado = context.Usuario
+                        .FirstOrDefault(u => u.usuarioId == UsuarioIdSeleccionado.Value);
+
+                    model.Ahorros = context.Ahorros
+                        .Where(a => a.usuarioId == UsuarioIdSeleccionado.Value)
+                        .ToList();
+
+                    model.Aportes = context.Aportes
+                        .Where(a => a.usuarioId == UsuarioIdSeleccionado.Value)
+                        .ToList();
+
+                    model.Prestamos = context.Prestamos
+                        .Where(p => p.usuarioId == UsuarioIdSeleccionado.Value)
+                        .ToList();
+
+                    model.Beneficios = context.BeneficiosServiciosCuenta
+                        .Where(b => b.usuarioId == UsuarioIdSeleccionado.Value)
+                        .ToList();
+                }
+
+            }
+
+
+            return View("EstadodeCuentaAsociados", model);
+        }
 
         //--------VISTAS USUARIO-------------//
-
-        public ActionResult GenerarEstadoAsociado()
-        {
-            return View();
-        }
-
+        // Si vas a tener una vista diferente para usuarios, puedes dejar esta vacía o eliminarla si no se usa
+        // public ActionResult GenerarEstadoAsociado()
+        // {
+        //     return View();
+        // }
     }
 }
---
+++ ASECCC Digital/ASECCC Digital/ViewModels/EstadoCuentaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ASECCC_Digital.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASECCC_Digital.ViewModels
+{
+    public class EstadoCuentaViewModel
+    {
+        public int? UsuarioIdSeleccionado { get; set; }
+        public Usuario UsuarioSeleccionado { get; set; }
+        public List<SelectListItem> Usuario { get; set; }
+        public List<Ahorros> Ahorros { get; set; }
+        public List<Aportes> Aportes { get; set; }
+        public List<Prestamos> Prestamos { get; set; }
+        public List<BeneficiosServiciosCuenta> Beneficios { get; set; }
+    }
+}
